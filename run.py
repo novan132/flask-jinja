0704d1d521e0fc303f2a327678a889e1e74391ec
lanovan132 <novanariadi@gmail.com>
2018-02-11 19:07:12 +0700

    done jinja

---
+++ run.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def template_test():
+    return render_template('template.html', my_string="Wheeee!", my_list=[0, 1, 2, 3, 4, 5], title='Home')
+
+@app.route('/home')
+def home():
+    return render_template('template.html', my_string="Wheeee!", my_list=[0, 1, 2, 3, 4, 5], title='Home')
+
+@app.route('/about')
+def about():
+    return render_template('template.html', my_string="Wheeee!", my_list=[0, 1, 2, 3, 4, 5], title='About')
+
+@app.route('/contact')
+def contact():
+    return render_template('template.html', my_string="Wheeee!", my_list=[0, 1, 2, 3, 4, 5], title='Contact Us')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
